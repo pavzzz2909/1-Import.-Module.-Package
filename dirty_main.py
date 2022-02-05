ffaf24878d14f9c0e577836b37c338c07953ec06
Pavel <pavzzz@yandex.ru>
2022-02-05 22:24:48 +0600

    4 задача

---
+++ dirty_main.py
@@ -0,0 +1,4 @@
+from main import *
+
+
+print(__name__)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from application.salary import calculate_salary
 from application.db.people import get_employees
 import datetime
 
+print(__name__)
+
 if __name__ == '__main__':
     print(f'Текущее точнейшее время {datetime.datetime.now()}')
     print()
